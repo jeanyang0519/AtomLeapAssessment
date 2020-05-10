e5915bc04d52be4447de46b4de2327f4658759d9
Jean <sy2803@tc.columbia.edu>
2020-05-10 12:19:30 -0400

    aggregate data

--- fundingviz/src/App.js
+++ fundingviz/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import BubbleChart from './components/bubbleChart';
 
 function App() {
   return (
     <div className="App">
-      
+      <BubbleChart/>
     </div>
   );
 }
--- fundingviz/src/components/bubbleChart.jsx
+++ fundingviz/src/components/bubbleChart.jsx
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
+import axios from 'axios';
 
 class BubbleChart extends Component {
     constructor(props) {
         super(props)
         
+        this.state = {
+            aggregatedData: []
+        }
     }
 
     componentDidMount() {
-        
+        axios.get("http://demo0377384.mockable.io/funding-test")
+            .then(res => {
+                let data = res.data;
+                let aggregatedData = d3.nest()
+                    .key(function (d) { return d.category; })
+                    .rollup(function (v) { return {
+                        rounds: v.length,
+                        total: d3.sum(v, function(d) { return d.fundingAmount; })
+                    }})
+                    .entries(data);
+
+                this.setState({ aggregatedData });
+                console.log(this.state.aggregatedData)
+            })
+
+            
     }
 
     render() {
@@ -17,3 +36,5 @@ class BubbleChart extends Component {
         )
     }
 }
+
+export default BubbleChart

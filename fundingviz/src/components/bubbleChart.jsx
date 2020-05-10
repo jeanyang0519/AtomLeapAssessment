cd7e240cd19e63888e7b78ce9a76817cb5ee2065
Jean <sy2803@tc.columbia.edu>
2020-05-10 11:52:24 -0400

    set up structure for BubbleChart

---
+++ fundingviz/src/components/bubbleChart.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+
+class BubbleChart extends Component {
+    constructor(props) {
+        super(props)
+        
+    }
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}

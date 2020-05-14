51e4fb1fd1e963d728fe5e6709f471a090d5a7a8
Jean <sy2803@tc.columbia.edu>
2020-05-14 14:07:09 -0400

    create table component

--- fundingviz/src/components/table.jsx
+++ fundingviz/src/components/table.jsx
@@ -1,1 +1,16 @@
 import React, { Component } from 'react';
+class Table extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Table;

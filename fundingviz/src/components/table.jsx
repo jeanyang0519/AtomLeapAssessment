d08b64fafa6b1dce0ab7226617bc31b1be7f7e6e
Jean <sy2803@tc.columbia.edu>
2020-05-14 14:09:43 -0400

    render table and header

--- fundingviz/src/components/table.jsx
+++ fundingviz/src/components/table.jsx
@@ -4,10 +4,38 @@ class Table extends Component {
         super(props)
     }
 
+    renderTable() {
+        return this.props.rows.map((info) => {
+            const { id, category, location,
+                fundingAmount, announcedDate } = info;
+            return (
+                <tr key={id}>
+                    <td>{id}</td>
+                    <td>{category}</td>
+                    <td>{location}</td>
+                    <td>{fundingAmount.toLocaleString()}</td>
+                    <td>{announcedDate}</td>
+                </tr>
+            )
+        })
+    }
+
+    renderHeader() {
+        let header = Object.keys(this.props.rows[0])
+        return header.map((key, index) => {
+            return <th key={index}>{key.toUpperCase()}</th>
+        })
+    }
+
     render() {
         return (
             <div>
-
+                <table>
+                    <tbody>
+                        <tr>{this.renderHeader()}</tr>
+                        {this.renderTable()}
+                    </tbody>
+                </table>
             </div>
         )
     }
